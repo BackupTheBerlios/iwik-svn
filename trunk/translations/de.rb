21ea2d9c59abd11014c7164658124f0338574a4d
dm1 <dm1@2b6c7695-b7f1-0310-a0a2-c6f085172375>
2005-04-07 18:34:31 +0000

    German translations (not complete)

---
+++ trunk/translations/de.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'i18nservice'
+
+I18nService::TABLE = {
+'All Pages' => 'Alle Seiten',
+'Authors' => 'Autor',
+'Back in time' => 'Zurück',
+'Cooking test' => 'Kochtest',
+'Edit' => 'Editieren',
+'Edit Page' => 'Seite Editieren',
+'Edit Web' => 'Konfigurieren',
+'Export' => 'Exportieren',
+# 'Feeds' => '',
+# 'Hide changes' => '',
+'Home Page' => 'Startseite',
+'Print' => 'Drucken',
+# 'Recently Revised' => '',
+'Search' => 'Suchen',
+'See changes' => 'Änderung sehen',
+'Test String' => 'Test Textfolge',
+'co- or authored:' => 'Co- or Autor von: '
+}
