ce0722db176f70036695c88ee35cb87b9fcf3eaa
dm1 <dm1@2b6c7695-b7f1-0310-a0a2-c6f085172375>
2005-04-07 18:21:48 +0000

    translation tests

---
+++ trunk/test/unit/translations_test.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby 
+
+require File.dirname(__FILE__) + '/../test_helper'
+
+require 'i18nservice'
+
+I18N = I18nService.instance
+
+class TranslationTest < Test::Unit::TestCase
+  # reset to default
+  def setup
+    I18N.lang = nil
+  end
+
+  def test_default_is_english
+    assert_equal('Test String', _('Test String'))
+  end
+  
+  def test_loading
+    I18N.lang = "fr"
+    assert_equal('Test de cuisson', _('Cooking test'))
+    
+    I18N.lang = "de"
+    assert_equal('Kochtest', _('Cooking test'))
+  end
+  
+  def test_utf8_chars
+    I18N.lang = "fr"
+    assert_equal('Chaîne de Test', _('Test String'))
+  end
+end
